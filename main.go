5c83998a7fc4d84e427d04e0fce6e6ff9abf4402
mainaksh <mainaksh@gmail.com>
2018-12-08 23:50:35 +0800

    updated main

--- main.go
+++ main.go
@@ -4,4 +4,21 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello go")
+
+	foo()
+	for i:=0; i<100; i++{
+		if i%2==0{
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+}
+
+func foo() {
+	fmt.Println("i m in foo")
+}
+
+func bar(){
+	fmt.Println("in bar exiting from main")
 }
